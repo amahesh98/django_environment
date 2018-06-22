d04b7dca51d9f0d7f1a37c57e85fd1bd7ba6226f
Ashwin <mahesh.ashwin1998@gmail.com>
2018-06-22 16:25:24 -0700

    Initial commit

---
+++ bin/flash.py
@@ -0,0 +1,4 @@
+#!/Users/Ashwin/Documents/CodingDojo/Python/MyEnvironments/djangoPy3Env/bin/python
+# EASY-INSTALL-SCRIPT: 'flash==1.0','flash.py'
+__requires__ = 'flash==1.0'
+__import__('pkg_resources').run_script('flash==1.0', 'flash.py')
